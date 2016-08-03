2c4df7e6ca5bccbe428379f5e5903458354bd281
zhangzijian <zhangzijian@syberos.com>
2016-08-03 13:36:44 +0800

    联系人变化

---
+++ GuardKiKi/src/main/java/com/pg/pgguardkiki/tools/ChatMessageListener.java
@@ -0,0 +1,14 @@
+package com.pg.pgguardkiki.tools;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.packet.Message;
+
+/**
+ * Created by zzj on 16-8-3.
+ */
+public class ChatMessageListener implements MessageListener {
+    public void processMessage(Chat var1, Message var2){
+
+    }
+}
--- GuardKiKi/src/main/java/com/pg/pgguardkiki/tools/SmackManagerTool.java
+++ GuardKiKi/src/main/java/com/pg/pgguardkiki/tools/SmackManagerTool.java
@@ -4,10 +4,19 @@ import android.util.Log;
 import com.pg.pgguardkiki.data.SystemData;
 import com.pg.pgguardkiki.service.ConnectService;
 
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.ChatManager;
 import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.Roster;
+import org.jivesoftware.smack.RosterEntry;
+import org.jivesoftware.smack.RosterListener;
+import org.jivesoftware.smack.SmackConfiguration;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.Presence;
+
+import java.util.Collection;
 
 /**
  * Created by zzj on 16-7-29.
@@ -17,6 +26,10 @@ public class SmackManagerTool{
     private XMPPConnection mConnection;
     private ConnectService mService;
     private static ConnectionConfiguration  mConnectionConfig;
+    private Roster mRoster;
+    private RosterListener mRosterListener;
+
+    private static final int PACKET_TIMEOUT = 30000;
 
     public SmackManagerTool(ConnectService service) {
 
@@ -30,11 +43,18 @@ public class SmackManagerTool{
         mConnection = new XMPPConnection(mConnectionConfig);
     }
 
+    public static void registerSmackProviders() {
+
+    }
+
     public boolean login(String phone, String password){
         if (mConnection.isConnected()) {
             mConnection.disconnect();
         }
         try {
+            SmackConfiguration.setPacketReplyTimeout(PACKET_TIMEOUT);
+            SmackConfiguration.setKeepAliveInterval(-1);
+            SmackConfiguration.setDefaultPingInterval(0);
             mConnection.connect();
         } catch (XMPPException e) {
             Log.d(ClassName, "Connect Error 0");
@@ -50,15 +70,39 @@ public class SmackManagerTool{
             Log.d(ClassName, "Login Error");
             return  false;
         }
+        registerRosterChangeListener();// 监听联系人动态变化
+
         // 更改在綫狀態
         Presence presence = new Presence(Presence.Type.available);
+        presence.setMode(Presence.Mode.available);
         mConnection.sendPacket(presence);
 
         return true;
     }
 
-    public static void registerSmackProviders() {
+    private void registerRosterChangeListener() {
+        mRoster = mConnection.getRoster();
+        mRosterListener = new RosterListener() {
+            @Override
+            public void presenceChanged(Presence presence) {
+                Log.d(ClassName, "presenceChanged 0= "+presence.getFrom());
+            }
 
+            @Override
+            public void entriesUpdated(Collection<String> entries) {
+                Log.d(ClassName, "entriesUpdated 1= "+entries);
+            }
+
+            @Override
+            public void entriesDeleted(Collection<String> entries) {
+                Log.d(ClassName, "entriesUpdated 2= "+entries);
+            }
+
+            @Override
+            public void entriesAdded(Collection<String> entries) {
+                Log.d(ClassName, "entriesAdded 2= "+entries);
+            }
+        };
+        mRoster.addRosterListener(mRosterListener);
     }
-
 }
